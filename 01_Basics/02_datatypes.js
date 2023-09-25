772acb21321cefb616eb4937f016f1e5bc788642
imvishalkrsingh <vishalkumarsingh162@gmail.com>
2023-09-25 16:18:03 +0530

    02_datatypes.js file added

---
+++ 01_Basics/02_datatypes.js
@@ -0,0 +1,23 @@
+
+
+console.log("Datatypes in Javascript")
+
+let name = "Vishal Kumar Singh"
+
+let age = 23
+
+let isLoggedIn = true
+
+// number => 2 to power of 53
+// bigInt => used in big companies
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined => decleared but not defined
+// symbol => Unique
+
+console.log(typeof "vishal");
+
+console.log("Type of null : " + typeof null)
+
+console.log("type of undefined : " + typeof undefined)
