c2b46f66a1bc3d8a665b67619ee809373fd63aa2
imvishalkrsingh <vishalkumarsingh162@gmail.com>
2023-09-25 19:51:23 +0530

    comparisions of operator

---
+++ 01_Basics/04_Comparision.js
@@ -0,0 +1,29 @@
+let a = 10
+
+let b = 20
+
+let c = "10"
+
+let name = "Vishal"
+
+console.log("Check a grater than b or not : ");
+console.log(a > b)
+
+console.log("Check a smaller than b or not : ");
+console.log(a < b)
+
+console.log("Check name grater than a : ");
+console.log(name > a);
+
+console.log("compare string to number : ");
+console.log(name > a);
+
+console.log("compare string to number : ");
+console.log(c < a);
+
+// strictly check operator
+
+console.log(c === a);
+
+
+
