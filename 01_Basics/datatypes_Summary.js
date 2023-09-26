602bd0a0a9a1e8179d8fed85715988dceb211744
imvishalkrsingh <vishalkumarsingh162@gmail.com>
2023-09-26 21:44:54 +0530

    primitive datatypes added

---
+++ 01_Basics/datatypes_Summary.js
@@ -0,0 +1,37 @@
+// datatypes : 1- Primitive Datatypes, 2- Non-Primitive Datatypes / Referenced Datatypes
+
+
+/* 
+Primitive Datatypes : String, number, boolean, null, undefined, bigInt
+
+Non-Primitive Datatypes : Array, object and function
+
+*/
+
+console.log("Primitive Datatypes : ");
+
+//  String datatypes
+let name = " Vishal Kumar Singh"
+console.log("String Value : " + name);
+
+// number
+let score = 495
+console.log("Number value : " + score);
+
+// boolean
+const isAdult = true
+console.log("Boolean Value : " + isAdult);
+
+// null
+let space = null
+console.log("Null value : " + space);
+
+// undefined
+let marks = undefined
+console.log("Undefined Value : " + marks);
+
+// bigInt
+let phnNumber = 98564558362953n
+console.log("BigInt Value : " + phnNumber);
+
+
