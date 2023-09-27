c143340e18576d22bfc10554a5d61f8ce03219df
imvishalkrsingh <vishalkumarsingh162@gmail.com>
2023-09-27 19:40:01 +0530

    string creation and functions

---
+++ 01_Basics/05_strings.js
@@ -0,0 +1,27 @@
+const name = "Vishal Singh"
+
+const repoCount = 50
+
+console.log(`My name is ${name} and I have ${repoCount} Repository in my Github Account.`);
+
+const gameName = new String('Cricket')
+
+console.log(gameName);
+
+console.log(typeof(gameName));
+
+// convert value into lowercase 
+console.log("Lowercase : " + gameName.toLowerCase());
+
+// convert value into uppercase 
+console.log("UpperCase : " + gameName.toUpperCase());
+
+// Trim function 
+console.log(`Trim : ${gameName.trim()}`);
+
+// charAt function
+console.log(`Check CharAt 3 : ${gameName.charAt(3)}`);
+
+// indexOf function
+console.log(`Check indexOf t : ${gameName.indexOf('t')}`);
+
