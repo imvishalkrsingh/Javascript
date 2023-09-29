0f05d02a7bd0afd6593a0b7953790a62d85d01f9
imvishalkrsingh <vishalkumarsingh162@gmail.com>
2023-09-29 21:48:20 +0530

    Dates in javascript

---
+++ 01_Basics/dates_in_javascript.js
@@ -0,0 +1,24 @@
+// Dates
+
+let myDate = new Date()
+console.log(myDate.toString());
+
+console.log('toDateString');
+console.log(myDate.toDateString());
+
+console.log('toLocalString');
+console.log(myDate.toLocaleString());
+
+console.log(typeof myDate);
+
+// created Date
+myCreatedDate = new Date(2023,0,23)
+console.log('Custom Date : ');
+console.log(myCreatedDate.toDateString());
+
+// Date.now
+
+let myTimeStamp = Date.now()
+
+
+
