b3c702b0d82128c302ed286df65465feceb5efa2
imvishalkrsingh <vishalkumarsingh162@gmail.com>
2023-10-03 22:17:04 +0530

    Compound Interest question  added

---
+++ Functions/Questions/Compound_Interest.js
@@ -0,0 +1,9 @@
+// Q. Write a function to calculate compound interest based on the principle amount
+
+function compoundInt(principle, rate, time, n) {
+    var interest = principle * (Math.pow((1 + (rate / n)), (n * time)));
+    return principle + interest;
+  }
+  
+  console.log(compoundInt(20000,5,2,2)) //3021250
+  console.log(compoundInt(150000,25,1,2)) //27487500
