933a38f48fc6d4fd3be95cedb0a98ff9416f8ab5
imvishalkrsingh <vishalkumarsingh162@gmail.com>
2023-10-03 22:15:10 +0530

    Simple Interest program file added

---
+++ Functions/Questions/Simple_Interest.js
@@ -0,0 +1,7 @@
+//  Q. Write a function to calculate simple interest based on the principle amount
+
+function simpleInterest(p,r,t){
+    return (p * r * t) / 100;
+}
+
+console.log(simpleInterest(20000,5,2));
