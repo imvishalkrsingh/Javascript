d22f1fd86b156210f9524aaee5f6a1186a8c46f3
imvishalkrsingh <vishalkumarsingh162@gmail.com>
2023-10-03 10:05:29 +0530

    Immediately invoked function expressions

---
+++ Functions/05_IIFE.js
@@ -0,0 +1,17 @@
+// IIFE -> immediately invoked function expressions (IIFE)
+
+/*
+An Immediately Invoked Function Expression (IIFE) is a JavaScript function that is executed immediately after it is defined. It is a way to create a private scope for variables and functions to avoid polluting the global scope. Here's the basic structure of an IIFE:
+*/
+
+(function printName(){
+    let name = "Vishal"
+    console.log(`My name is ${name}`);
+})();
+
+// Immediately invoked function expressions in Arrow function
+
+((phone) => {
+    // let phone = 'Realme'
+    console.log(`My phone is ${phone}`);
+})('Apple');
