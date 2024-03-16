789ff481252ffa3761356732640b8db7aea9e105
imvishalkrsingh <vishalkumarsingh162@gmail.com>
2024-03-16 14:04:31 +0530

    Questions of strings

--- JS - Apna College/05_Strings/05.2_Questions/script.js
+++ JS - Apna College/05_Strings/05.2_Questions/script.js
@@ -1,6 +1,8 @@
 // Practice Questions
 
 // Q1. Prompt the user to enter their name. Generate a username for them based on the input. Start username with @, followed by their full name and ending with the fullname length.
+let text = "We are the so-called \"Vikings\" from the north.";
+console.log(text);
 
 let fullName = prompt("Enter your full name : ")
 console.log("Full name is : ", fullName);
