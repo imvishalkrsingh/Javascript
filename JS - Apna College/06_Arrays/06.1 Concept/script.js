1b80e734b5244c17b07d068c0dea0fff9dc8ba01
imvishalkrsingh <vishalkumarsingh162@gmail.com>
2024-03-17 12:36:13 +0530

    Arrays concept

--- JS - Apna College/06_Arrays/06.1 Concept/script.js
+++ JS - Apna College/06_Arrays/06.1 Concept/script.js
@@ -1,5 +1,7 @@
 // Arrays in Javascript
 
+
+
 let marks_Student1 = 97;
 let marks_Student2 = 96;
 let marks_Student3 = 95;
@@ -7,6 +9,16 @@ let marks_Student4 = 94;
 let marks_Student5 = 93;
 
 // Arrays = collections of items
+
+let car1 = "Saab";
+let car2 = "Volvo";
+let car3 = "BMW";
+
+const cars = [];
+cars[0]= "Saab";
+cars[1]= "Volvo";
+cars[2]= "BMW";
+
 
 // Creating an Array -
 
@@ -35,6 +47,58 @@ console.log("------for-of Loop------");
 for(let el of heroes){
     console.log(el);
 }
+
+// Array Methods -----
+
+// push() : add to end
+
+let foodItems = ["mango", "apple", "litchi", "grapes"]
+console.log(foodItems);
+
+foodItems.push("Oranges")
+console.log(foodItems);
+
+// pop()  : delete from end and return
+
+foodItems.pop();
+console.log(foodItems);
+
+// toString() : converts array to string
+console.log(foodItems.toString());
+
+// concat() ; Joins multiple arrays and returns result
+
+let vegetables = ["Potato", "tomato", "cabbage"]
+
+console.log(foodItems.concat(vegetables));
+
+
+// unshift() : add to start
+vegetables.unshift("chilli")
+console.log(vegetables);
+
+// unshift() :Delete elements from start
+
+vegetables.shift();
+console.log(vegetables);
+
+// slice() : returns a piece of the array
+
+console.log(foodItems.slice(1,3));
+
+// splice() : change original array (add, remove, replace)
+
+let arr = [1, 2, 3, 4, 5, 6]
+
+arr.splice(2,2,101, 102)
+console.log(arr);
+
+arr.splice(3, 1, 103)
+
+console.log(arr);
+
+
+
 
 
 
--- JS - Apna College/06_Arrays/06.2 Questions/script.js
+++ JS - Apna College/06_Arrays/06.2 Questions/script.js
@@ -1,6 +1,7 @@
 // Let's practice some Question of Arrays in javascript
 
-// Q1. For a given array with marks of students -> [85,97,44,37,76,60] Find the average marks of the entire class.
+// Q1. For a given array with marks of students -> 
+// [85,97,44,37,76,60] Find the average marks of the entire class.
 
 let marks = [85, 97, 44, 37, 76, 60];
 let sum = 0;
@@ -15,13 +16,40 @@ console.log("Average of 6 Students in a class :", avg);
 
 // Q2. For a given array with prices of 5 items -> [250, 645, 300, 900, 50] All items have an offer of 10% off on them. Change the array to store final price after applying offer.
 
-let price = [ 250, 645, 300, 900, 50];
-let price1 = [];
+let items = [250, 645, 300, 900, 50];
 
-for (let i = 0; i < price.length; i++) {
-    let offer = i / 10;
-    const element = price[i];
-    console.log(element);
-    price[i] = price[i] - offer;
-    console.log(`Value after offer = ${price[i]}`);
+// let i = 0;
+// for(let val of items){
+//     console.log(`value at index ${i} = ${val}`);
+//     let offer = val / 10;
+//     items[i] = items[i] - offer;
+//     console.log(`value after offer = ${items[i]}`);
+//     i++;
+// }
+
+// using for loop to solve this problem....
+
+for(let i = 0; i < items.length; i++){
+    let offer = items[i] / 10;
+    items[i] -= offer;
 }
+
+console.log(items);
+
+
+// Q3. Create an array to store companies -> "Bloomberg", "Microsoft", "Uber", "Google", "IBM", "Netflix"
+
+let companies = ["Bloomberg", "Microsoft", "Uber", "Google", "IBM", "Netflix"];
+console.log(companies);
+
+// a. Remove the first company from the array.
+// companies.shift()
+// console.log(companies);
+
+// b. Remove uber and add Ola in its place.
+companies.splice(2, 1, "Ola")
+console.log(companies);
+
+// Add Amazon at the end.
+companies.push("Amazon");
+console.log(companies);
