eeee9a48ec05653e785c6d9eaf66005b5a90a9c8
imvishalkrsingh <vishalkumarsingh162@gmail.com>
2024-03-18 23:36:38 +0530

    Functions in Javascript

---
+++ JS - Apna College/07_Functions/07.1_Concept/script.js
@@ -0,0 +1,14 @@
+console.log("Functions in JS");
+
+// Functions in JS -- A JavaScript function is a block of code designed to perform a particular task.
+
+// Function definition --
+// function functionName(param1, param2){
+    // do some work
+// }
+
+// function call --
+// functionName();
+
+
+
