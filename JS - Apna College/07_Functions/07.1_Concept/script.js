1e3ee2ab93ebd19f638d5cc9c14eeff8af40b034
imvishalkrsingh <vishalkumarsingh162@gmail.com>
2024-03-19 14:49:28 +0530

    Functions in javascript

--- JS - Apna College/07_Functions/07.1_Concept/script.js
+++ JS - Apna College/07_Functions/07.1_Concept/script.js
@@ -1,14 +1,41 @@
-console.log("Functions in JS");
+// JavaScript functions are essential blocks of code designed to perform specific tasks, promoting code reusability and organization.
 
-// Functions in JS -- A JavaScript function is a block of code designed to perform a particular task.
-
-// Function definition --
-// function functionName(param1, param2){
-    // do some work
+// Function definition syntax:
+// function functionName(parameter1, parameter2, ...) {
+//     // code block to execute
 // }
 
-// function call --
-// functionName();
+// Function call syntax:
+// functionName(argument1, argument2, ...);
 
+// Example of a simple function:
+function myFunction() {
+    console.log("Welcome to Apna College");
+    console.log("We are learning JavaScript");
+}
 
+// Calling the function:
+myFunction();
 
+// Function with parameters:
+function welcome(name) {  // 'name' is a parameter
+    console.log("Hello", name);
+}
+
+// Calling the function with an argument:
+welcome("Vishal Singh"); // Output: Hello Vishal Singh
+
+// Creating a function to calculate the sum of two numbers:
+function sum(a, b) {
+    console.log("The sum of", a, "and", b, "is:", a + b);
+}
+
+// Calling the sum function:
+sum(4, 2); // Output: The sum of 4 and 2 is: 6
+
+// Functions can have comments to explain their purpose, parameters, and return values.
+// Comments provide clarity and make the code more understandable to others.
+
+// JavaScript functions can return values using the 'return' keyword. This allows functions to produce results for further computation.
+
+// Functions can also be assigned to variables, passed as arguments to other functions, or even returned from other functions, enabling powerful functional programming paradigms.
