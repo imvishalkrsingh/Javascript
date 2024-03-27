8d19b39c81883beb96eb33152dce809d7218a0e8
imvishalkrsingh <vishalkumarsingh162@gmail.com>
2024-03-27 22:06:21 +0530

    Call back question

--- JS - Apna College/07_Functions/07.2_Questions/script.js
+++ JS - Apna College/07_Functions/07.2_Questions/script.js
@@ -198,3 +198,25 @@ function sayHello() {
 let sayGoodbye = function() {
     console.log("Goodbye, world!");
 };
+
+/*
+Question 11:
+Explain the concept of a callback function in JavaScript and provide an example of its usage. Describe a scenario where callback functions are commonly employed and discuss their significance in asynchronous programming.
+*/
+// Example of a callback function
+function fetchData(url, callback) {
+    // Simulating asynchronous data fetching
+    setTimeout(function() {
+        const data = "Some data retrieved from " + url;
+        // Invoke the callback function with the retrieved data
+        callback(data);
+    }, 2000); // Simulating a delay of 2 seconds
+}
+
+// Callback function to handle the retrieved data
+function handleData(data) {
+    console.log("Data received:", data);
+}
+
+// Using fetchData function with a callback
+fetchData("https://example.com/api/data", handleData);
