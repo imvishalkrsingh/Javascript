d16ade2a377dec6d4e44f1ce9012119f258e7541
imvishalkrsingh <vishalkumarsingh162@gmail.com>
2024-04-08 20:39:59 +0530

    DOM accessing element

---
+++ JS - Apna College/08_DOM/08.1_Concept/script.js
@@ -0,0 +1,18 @@
+/*
+In JavaScript, the Document Object Model (DOM) is a programming interface that represents the structure of a web page as a tree-like structure, where each node represents an element (such as a paragraph, a heading, or an image) and each branch represents the relationships between elements. The DOM provides a way for scripts to dynamically access and manipulate the content, structure, and style of a web page.
+*/
+
+/*
+1.Accessing Elements:
+You can access elements in the DOM using methods like document.getElementById(), document.querySelector(), or document.querySelectorAll(). These methods allow you to select elements by their ID, class, tag name, or other attributes.
+*/
+
+// Get an element by ID
+var element = document.getElementById("myElementId");
+
+// Get the first element with a specific class
+var element = document.querySelector(".myClass");
+
+// Get all elements with a specific class
+var elements = document.querySelectorAll(".myClass");
+
