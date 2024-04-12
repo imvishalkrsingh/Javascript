a3b692dd1b8c69bc66c162ab101002019e0e4f1b
imvishalkrsingh <vishalkumarsingh162@gmail.com>
2024-04-12 21:10:46 +0530

    DOM part 2 completed

--- JS - Apna College/08_DOM/08.3_DOM Part2/script.js
+++ JS - Apna College/08_DOM/08.3_DOM Part2/script.js
@@ -32,5 +32,29 @@ h1.after(el); // adds after the node (outside)
 
 el.remove(); // removes the node
 
+// Q1. create a new button element. Give it a text "Click me", background color of red and text color of white.
+// insert the button as the first element inside the body tag.
+
+let clickMe = document.createElement("button");
+clickMe.innerText = "click me!";
+
+clickMe.style.color = 'white';
+clickMe.style.backgroundColor = 'red';
+
+document.querySelector("body").prepend(clickMe);
+
+// Q2. Create a paragraph tag in html, give it a class and some styling. Now create a new class in CSS and try to append this class to the paragraph element.
+
+// Did you notice, how you overwrite the class name when you add a new one ? Solve this problem using classList.
+
+let para1 = document.getElementsByClassName("content")[0];
+
+console.log(para1.getAttribute("class"));
+
+para1.classList.add("newClass"); // Adding new class using classList
+
+
+
+
 
 
