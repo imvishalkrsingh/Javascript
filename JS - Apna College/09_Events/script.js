07aa208145fe4438a475ee4d5492a3e336ba3db9
imvishalkrsingh <vishalkumarsingh162@gmail.com>
2024-04-13 23:22:08 +0530

    Events in Javascript

---
+++ JS - Apna College/09_Events/script.js
@@ -0,0 +1,10 @@
+// Events in Javascript
+
+let btn1 = document.getElementById("btn1");
+btn1.onclick = () => {
+    console.log("btn1 was clicked");
+    let a = 25;
+    a++;
+    console.log(a);
+}
+
